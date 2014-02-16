8afa28b743d8ce824dc5bb716590d537c4d85f06
lamawe <wes.lamarche@mchsi.com>
2014-02-15 20:20:20 -0600

    Initial Project Commit.  Contains a readme file and a HelloWorld class

---
+++ HelloWorld/Readme.md
@@ -0,0 +1,11 @@
+RAB_I
+=====
+
+First Project: Collaborate with Matt
+"Room and Board - Semester I"
+
+Project Structure
+
+Source Files: ./src
+Binary Files: ./bin
+
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * A simple file to prove we can collaborate
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
